e91336778113f2edb7d8d0f6d63c0b833de8486c
BC <b.wayne@me.com>
2019-02-17 20:02:31 -0700

    create and read complete

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "149e996d9712c9c5b836";
+/******/ 	var hotCurrentHash = "9f7d1ef3b6f556e4b0be";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

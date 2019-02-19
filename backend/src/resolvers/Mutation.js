8ef7a85d637c4596c8f36aec45ad594083876fb4
BC <b.wayne@me.com>
2019-02-19 12:22:24 -0700

    created a relationship in the db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,12 +2,22 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { randomBytes } = require("crypto");
 const { promisify } = require("util");
+const { transport, makeANiceEmail } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    const item = ctx.db.mutation.createItem(
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in to do that!");
+    }
+    const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This is how to create a relationship between the item and the user.
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args
         }
       },
@@ -97,8 +107,20 @@ const Mutations = {
       data: { resetToken, resetTokenExpiry }
     });
     console.log(res);
-    return { message: "thanks" };
     // Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: "ben@ben.com",
+      to: user.email,
+      subject: "Your password reset token",
+      html: makeANiceEmail(`Your password reset token is here.
+      \n\n
+      <a href="${
+        process.env.FRONTEND_URL
+      }/reset?resetToken=${resetToken}">Click here to reset</a>
+      `)
+    });
+    // return the message
+    return { message: "thanks" };
   },
   async resetPassword(parent, args, ctx, info) {
     //check if the passwords match

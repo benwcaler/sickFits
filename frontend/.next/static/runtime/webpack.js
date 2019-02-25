89482ffc956330c3877474d83bd19d8475f7ec92
BC <b.wayne@me.com>
2019-02-24 21:11:49 -0700

    removing items from cart

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "91b26af6752d0ef8366d";
+/******/ 	var hotCurrentHash = "3598b40f39e42aea1764";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

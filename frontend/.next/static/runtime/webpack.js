eb2e2378aad547d4667b6cf9c1ce7d3f4ad7acfc
BC <b.wayne@me.com>
2019-02-26 20:06:11 -0700

    updating

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a3736d29525c8411fc02";
+/******/ 	var hotCurrentHash = "cefd51f800072b07cc11";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
